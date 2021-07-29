0560f37f9fed21fe49219f561aec29dbb644da08
Francielxyz <ruanvha15@gmail.com>
2021-07-28 21:21:12 -0300

    crud de marcas

--- src/main/java/com/lojafran/Loja_Franciel/controller/FuncionarioController.java
+++ src/main/java/com/lojafran/Loja_Franciel/controller/FuncionarioController.java
@@ -1,17 +1,15 @@
 package com.lojafran.Loja_Franciel.controller;
 
-
-
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.lojafran.Loja_Franciel.model.Funcionario;
@@ -19,48 +17,47 @@ import com.lojafran.Loja_Franciel.repository.FuncionarioRepository;
 
 
 @Controller
+@RequestMapping("/administrativo/funcionarios")
 public class FuncionarioController {
 
 	@Autowired
 	private FuncionarioRepository funcionarioRepositorio;
 	//melhorar forma do caminho (rota) relacionando ao controler
 	
-	@GetMapping("/administrativo/funcionarios/cadastrar")
+	@GetMapping("/cadastrar")
 	public ModelAndView cadastrar(Funcionario funcionario) {
 		ModelAndView mv = new ModelAndView("administrativo/funcionarios/cadastro");
 		mv.addObject("funcionario",funcionario);
 		return mv;
 	}
 	
-	@GetMapping("/administrativo/funcionarios/listar")
+	@GetMapping("/listar")
 	public ModelAndView listar() {
 		ModelAndView mv = new ModelAndView("/administrativo/funcionarios/lista");
 		mv.addObject("listaFuncionarios", funcionarioRepositorio.findAll());
 		return mv;
 	}
 	
-	@GetMapping("/administrativo/funcionarios/editar/{id}")
+	@GetMapping("/editar/{id}")
 	public ModelAndView editar(@PathVariable("id") Long id) {
 		Optional<Funcionario> funcionario = funcionarioRepositorio.findById(id);
 		return cadastrar(funcionario.get());
 	}
 	
-	@GetMapping("/administrativo/funcionarios/remover/{id}")
+	@GetMapping("/remover/{id}")
 	public ModelAndView remover(@PathVariable("id") Long id) {
 		Optional<Funcionario> funcionario = funcionarioRepositorio.findById(id);
 		funcionarioRepositorio.delete(funcionario.get());
 		return listar();
 	}
 	
-	@PostMapping("/administrativo/funcionarios/salvar")
+	@PostMapping("/salvar")
 	public ModelAndView salvar(@Validated Funcionario funcionario, BindingResult result) {
 		if(result.hasErrors()) {
-	
 			return cadastrar(funcionario);
 		}
 		
 		funcionarioRepositorio.saveAndFlush(funcionario);
-		
 		return cadastrar(new Funcionario());
 	}
 	
---
+++ src/main/java/com/lojafran/Loja_Franciel/repository/MarcaRepository.java
@@ -0,0 +1,9 @@
+package com.lojafran.Loja_Franciel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lojafran.Loja_Franciel.model.Marca;
+
+public interface MarcaRepository extends JpaRepository<Marca, Long>{
+
+}
