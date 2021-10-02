ad48966f5f9ee4936c3b42d224f0334f9ee25119
Francielxyz <ruanvha15@gmail.com>
2021-10-01 21:58:14 -0300

    Implementação para cadastrar Papeis dos funcionários

---
+++ src/main/java/com/lojafran/Loja_Franciel/repository/PapelRepository.java
@@ -0,0 +1,8 @@
+package com.lojafran.Loja_Franciel.repository;
+
+import com.lojafran.Loja_Franciel.model.Papel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PapelRepository extends JpaRepository<Papel, Long>{
+
+}
