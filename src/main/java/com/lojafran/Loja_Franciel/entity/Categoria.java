39237101fb61db6d80b533890163f017b36c9ee4
Francielxyz <ruanvha15@gmail.com>
2021-10-09 11:57:18 -0300

    Criação entidade Categoria

---
+++ src/main/java/com/lojafran/Loja_Franciel/entity/Categoria.java
@@ -0,0 +1,31 @@
+package com.lojafran.Loja_Franciel.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categoria")
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
